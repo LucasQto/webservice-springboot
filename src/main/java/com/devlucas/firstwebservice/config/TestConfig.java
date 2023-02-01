4066272c251fef3bd4403cd3b669729c56efa23f
LucasQto <lucasquinto22@gmail.com>
2023-02-01 14:20:47 -0300

    OrderStatus enum

--- src/main/java/com/devlucas/firstwebservice/config/TestConfig.java
+++ src/main/java/com/devlucas/firstwebservice/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.devlucas.firstwebservice.config;
 
 import com.devlucas.firstwebservice.model.Order;
 import com.devlucas.firstwebservice.model.User;
+import com.devlucas.firstwebservice.model.enums.OrderStatus;
 import com.devlucas.firstwebservice.repository.OrderRepository;
 import com.devlucas.firstwebservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "tst", "tst@gmail.com", "3263324363", "372844348374");
         User u2 = new User(null, "teste", "teste@gmail.com", "326343346263", "372434848374");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
