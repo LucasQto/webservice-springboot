044a269293144b4ca294e1d7153780a3562fbc1e
LucasQto <lucasquinto22@gmail.com>
2023-02-01 17:02:56 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/devlucas/firstwebservice/model/Product.java
+++ src/main/java/com/devlucas/firstwebservice/model/Product.java
@@ -1,5 +1,7 @@
 package com.devlucas.firstwebservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -16,11 +18,15 @@ public class Product implements Serializable {
     private String description;
     private Double price;
     private String imgUrl;
+
     @ManyToMany
     @JoinTable(name = "tb_product_category",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){}
 
@@ -74,6 +80,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
