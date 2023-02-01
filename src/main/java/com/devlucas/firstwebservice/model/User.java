8f4655a093f0ebf281ec675e19d860c08591a010
LucasQto <lucasquinto22@gmail.com>
2023-02-01 11:58:14 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/devlucas/firstwebservice/model/User.java
+++ src/main/java/com/devlucas/firstwebservice/model/User.java
@@ -1,7 +1,11 @@
 package com.devlucas.firstwebservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +19,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
     }
@@ -65,6 +72,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
---
+++ src/main/java/com/devlucas/firstwebservice/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.devlucas.firstwebservice.service;
+
+import com.devlucas.firstwebservice.model.Order;
+import com.devlucas.firstwebservice.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
