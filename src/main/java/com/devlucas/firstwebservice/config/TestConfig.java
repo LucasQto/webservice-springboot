967253f4027c9ab171e298f317382a2808fbbd7c
LucasQto <lucasquinto22@gmail.com>
2023-02-01 16:44:38 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/devlucas/firstwebservice/config/TestConfig.java
+++ src/main/java/com/devlucas/firstwebservice/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.devlucas.firstwebservice.config;
 
-import com.devlucas.firstwebservice.model.Category;
-import com.devlucas.firstwebservice.model.Order;
-import com.devlucas.firstwebservice.model.Product;
-import com.devlucas.firstwebservice.model.User;
+import com.devlucas.firstwebservice.model.*;
 import com.devlucas.firstwebservice.model.enums.OrderStatus;
-import com.devlucas.firstwebservice.repository.CategoryRepository;
-import com.devlucas.firstwebservice.repository.OrderRepository;
-import com.devlucas.firstwebservice.repository.ProductRepository;
-import com.devlucas.firstwebservice.repository.UserRepository;
+import com.devlucas.firstwebservice.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -68,5 +65,14 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p4.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p1.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+
     }
 }
